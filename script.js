79337105b9bf5110f980bd945fc246b10fb1d311
Shackile Ally Asuncion <shackileallyasuncion@Ally-3.local>
2023-04-14 15:36:26 +0800

    second commit

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var selectedRow = null;
 
 function onFormSubmit(e) { // "e" is used as a parameter which is short for event
         event.preventDefault(); // "e" is not used as shortcut, use "event" instead.
-        var formData = readFormData(); // assigning a variable which will contain the objects from the function
-        if(selectedRow === null){
-                insertNewRecord(formData);
-        } else{
-                updateRecord(formData);
-        }
-
+                var formData = readFormData(); // assigning a variable which will contain the objects from the function
+                if(selectedRow == null){
+                        insertNewRecord(formData);
+                } else {
+                        updateRecord(formData);
+                }
+                resetForm();
 }
 
 
@@ -28,16 +28,16 @@ function readFormData(){ // insert the data from form to the blank Object
 function insertNewRecord(data) {
         var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
         var newRow = table.insertRow(table.length);
-        var cell1 = newRow.insertCell(0);
+        cell1 = newRow.insertCell(0);
                 cell1.innerHTML = data.productCode;
-        var cell2 = newRow.insertCell(1);
+        cell2 = newRow.insertCell(1);
                 cell2.innerHTML = data.product;
-        var cell3 = newRow.insertCell(2);
+        cell3 = newRow.insertCell(2);
                 cell3.innerHTML = data.quantity;
-        var cell4 = newRow.insertCell(3);
+        cell4 = newRow.insertCell(3);
                 cell4.innerHTML = data.perPrice;
-        var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button onClick = onEdit(this) >Edit</button><button>Delete</button>`;
+        cell5 = newRow.insertCell(4);
+                cell5.innerHTML = `<button onClick = "onEdit(this)">Edit</button><button onClick = "onDelete(this)">Delete</button>`;
 }
 
 // Editing the data
@@ -47,7 +47,7 @@ function onEdit(td) {
         document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
         document.getElementById("product").value = selectedRow.cells[1].innerHTML;
         document.getElementById("quantity").value = selectedRow.cells[2].innerHTML;
-        document.getElementById("perPrice").value = selectedRow.cells[4].innerHTML;
+        document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
 }
 
 function updateRecord(formData) {
@@ -63,7 +63,15 @@ function onDelete(td) {
         if ( confirm("Do you want to delete this record?") ) {
                 row = td.parentElement.parentElement;
                 document.getElementById('storelist').deleteRow(row.rowIndex);
-        }
+                resetForm();
+        } 
 }
 
 // Reset the data 
+function resetForm() {
+        document.getElementById("productCode").value = '';
+        document.getElementById("product").value = '';
+        document.getElementById("quantity").value = '';
+        document.getElementById("perPrice").value = '';
+        selectedRow = null;
+}
